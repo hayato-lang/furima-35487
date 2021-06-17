ec6552537b041295a0594502a5483685620b2de7
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-06-17 20:01:11 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
   validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with:PASSWORD_REGEX
 end
