cfb52b418a69343019d4fae1f22d50467bc871f9
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-06-20 19:16:04 +0900

    販売手数料や販売利益の改正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,11 +1,12 @@
 function price() {
   const itemPrice = document.getElementById('item-price');
-  itemPrice.addEventListener('keyup', () => {
+  itemPrice.addEventListener('input', () => {
     const addTaxPrice = document.getElementById('add-tax-price');
     const profit = document.getElementById('profit');
+    const itemValue = itemPrice.value
 
-    addTaxPrice.innerHTML = Math.floor(itemPrice.value * 0.10)
-    profit.innerHTML = itemPrice.value - addTaxPrice.innerHTML 
+    addTaxPrice.innerHTML = Math.floor(itemValue * 0.10)
+    profit.innerHTML = itemValue - addTaxPrice.innerHTML 
   });
 };
 
