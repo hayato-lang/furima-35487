4e99eece8c692ba2769cbed460a6de79cd13e337
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-06-20 17:44:07 +0900

    ビューファイル関連を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :item
   has_one_attached :image
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+  belongs_to :status
+
+  with_options numericality: {other_than: 1} do
+    validates :category_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :status_id
+  end
 end
