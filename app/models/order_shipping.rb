4a97193cf6128ea04e508bd442133c80563bc124
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-06-27 14:35:40 +0900

    購入ページにおける商品詳細の実装

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,13 +1,19 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :municipality, :address, :building_name, :phone_number, :user_id
+  attr_accessor :postal_code, :prefecture, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :user_id
+    validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :municipality
     validates :address
     validates :phone_number
   end
   validates :prefecture_id, numericality: {other_than: 0}
+
+  def save
+    @order = Order.create(user_id: user_id, item_id: item_id)
+    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
