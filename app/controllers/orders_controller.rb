d780883b66887ea2494a41849122ad524c228177
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-06-27 16:17:36 +0900

    ユーザー認証の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_order, only: [:index, :create]
+  before_action :user_authentication, only: [:index, :create]
   def index 
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new
   end
 
@@ -17,5 +19,15 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_shipping).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
+
+  def user_authentication
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
