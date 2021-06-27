ae962e5c66e71b689ee963a5e2f0c15ba17b641c
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-06-27 18:47:44 +0900

    クレジットカード決済機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @order_shipping = OrderShipping.new(order_params)
     if @order_shipping.valid?
+      pay_item
       @order_shipping.save
       redirect_to root_path
     else
@@ -18,7 +19,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number,).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 
   def set_order
@@ -28,6 +29,15 @@ class OrdersController < ApplicationController
   def user_authentication
     if current_user.id == @item.user_id
       redirect_to root_path
+    end
+
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
     end
   end
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,6 +1,6 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :user_id
@@ -10,10 +10,11 @@ class OrderShipping
     validates :address
     validates :phone_number
     validates :prefecture_id, numericality: {other_than: 1}
+    validates :token
   end
 
   def save
     @order = Order.create(user_id: user_id, item_id: item_id)
-    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Shipping.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: @order.id)
   end
 end
